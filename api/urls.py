654820a19cce752a802ffe12e3b0a2c07bf8499d
Михаил <Mikevlg34@yandex.ru>
2021-04-20 23:00:14 +0300

    first

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -11,5 +12,5 @@ router.register('group', views.GroupViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path()
+    path('v1/api-token-auth/', views.obtain_auth_token)
 ]
