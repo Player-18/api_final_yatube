81e83a25d57c2a4dd2992c8067a1b4eac78de5d0
Михаил <Mikevlg34@yandex.ru>
2021-04-20 22:48:26 +0300

    first

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,57 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
-from .models import Comment, Post
+from .models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+    )
 
     class Meta:
-        fields = ('id', 'text', 'author', 'pub_date')
+        fields = '__all__'
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+    )
 
     class Meta:
-        fields = ('id', 'author', 'post', 'text', 'created')
+        fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+    )
+
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all())
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError('Нельзя подписаться на себя')
+        return data
+
+    class Meta:
+        fields = ('id', 'user', 'following')
+        model = Follow
+        validators = [UniqueTogetherValidator(
+                      queryset=Follow.objects.all(),
+                      fields=['user', 'following'],
+                      message='Нельзя подписаться еще раз')]
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'title')
+        model = Group
