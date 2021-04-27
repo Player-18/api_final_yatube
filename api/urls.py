fb2e44ae667d68db8c0849b340dc729a86509185
Михаил <Mikevlg34@yandex.ru>
2021-04-28 00:15:07 +0300

    Maybe

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView, 
-                                            TokenRefreshView) 
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from . import views
 
@@ -13,8 +13,8 @@ router.register('group', views.GroupViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('api/v1/token/', TokenObtainPairView.as_view(), 
-         name='token_obtain_pair'), 
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), 
+    path('api/v1/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(),
          name='token_refresh')
 ]
