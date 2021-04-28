8c7c4fa763d6dde8a91f05d1adcec2230860044b
Михаил <Mikevlg34@yandex.ru>
2021-04-28 20:39:34 +0300

    Last

--- api/urls.py
+++ api/urls.py
@@ -13,8 +13,8 @@ router.register('group', views.GroupViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('api/v1/token/', TokenObtainPairView.as_view(),
+    path('v1/token/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(),
+    path('v1/token/refresh/', TokenRefreshView.as_view(),
          name='token_refresh')
 ]
